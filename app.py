620ef60dfc2a58e68efdbe41365c1db34c8a7d36
HSapers <hsapers@caltech.edu>
2023-05-12 16:24:16 -0400

    added dynamic title

--- app.py
+++ app.py
@@ -18,16 +18,23 @@ metric_list = list(df['metric'].unique())
 
 #widgets
 with st.sidebar: #implements side bar function and do that with these things
+    st.subheader("Configure the plot")
     continent = st.selectbox(label='Choose a continent', options=continent_list)
     metric = st.selectbox(label='Choose a metric', options=metric_list)
 
 #query
 query = f"continent=='{continent}' & metric=='{metric}'"                        
 
-#df_gdp_o = df.query("continent=='Oceania' & metric=='gdpPercap'") #hardcoded
+#df_gdp_o = df.query("continent=='Oceania' & metric=='gdpPercap'") hardcoded
 #df_gdp_o = df.query(f"continent=='{continent}' & metric=='{metric}'") #use variables
-df_gdp_o = df.query(query) #use more variables
 
-fig = px.line(df_gdp_o, x = "year", y = "value", color = 'country', title='GDP for countries in Oceania')
+df_gdp = df.query(query) #use more variables
+metric_labels = {'gdpPercap' : 'GDP Per Capita', 'lifeExp' : 'Average Life Expectancy', 'pop' : 'Population'} #make the metric labels nice
+#title = 'GDP for countries in Oceania' #hard coded
+title = f"{metric_labels[metric]} for countries in {continent}"
+
+
+
+fig = px.line(df_gdp, x = "year", y = "value", color = 'country', title=title)
 
 st.plotly_chart(fig, use_container_width=True)
